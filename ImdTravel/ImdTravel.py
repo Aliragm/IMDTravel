c1e0800e83a0dad95410bb376ea41cbb80806d29
André Lira <andrelira50@gmail.com>
2025-10-24 20:42:05 -0300

    Adicionando arquivos de config do docker junto do start dos microserviços

---
+++ ImdTravel/ImdTravel.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Olá de ImdTravel!"
+
+if __name__ == '__main__':
+    # ISSO É OBRIGATÓRIO PARA O DOCKER FUNCIONAR:
+    app.run(host='0.0.0.0', port=5000)
