84f7931d7e66dd1e79d65b4e10e6121709920f99
ulissesdamasceno7 <ufolador@gmail.com>
2025-11-06 19:16:46 -0300

    requests 2 e 4

--- Exchange/Exchange.py
+++ Exchange/Exchange.py
@@ -1,10 +1,29 @@
 import flask
 import random
+import time
 
 app = flask.Flask(__name__)
 
+fail_state_active = False
+fail_until_timestamp = 0
+
 @app.route('/exchange', methods=['GET'])
 def get_exchange_rate():
+    global fail_state_active, fail_until_timestamp
+
+    current_time = time.time()
+
+    if fail_state_active:
+        if current_time < fail_until_timestamp:
+            return flask.jsonify({"error": "Simulated Server Error"}), 500
+        else:
+            fail_state_active = False
+
+    if random.random() < 0.1:
+        fail_state_active = True
+        fail_until_timestamp = current_time + 5
+        return flask.jsonify({"error": "Simulated Server Error"}), 500
+
     rate = random.uniform(5.0, 6.0)
     return flask.jsonify({"value": rate}), 200
 
